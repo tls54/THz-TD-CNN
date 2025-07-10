1d5d97fbf46aa62cae5eeb09eb1045f0cdef250b
Theo Smith <tls54@outlook.com>
2025-07-10 16:58:04 +0100

    Refactored models to prepare for Auto-encoder tests

--- models/Classifier_models.py
+++ models/Classifier_models.py
@@ -3,11 +3,6 @@ import torch.nn as nn
 from tqdm import tqdm
 
 
-def identify_device():
-    device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
-    print(f"Using device: {device}")
-    return device
-
 # TODO: Write in a verbose flag for long epoch training
 
 ## First basic CNN 
