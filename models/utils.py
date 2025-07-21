e37605b94547b9756fee240d609347a0dde9faa2
Theo Smith <tls54@outlook.com>
2025-07-21 12:48:11 +0100

    Tested new model hyper params. refactored some utils.

--- models/utils.py
+++ models/utils.py
@@ -6,12 +6,24 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, accuracy_score, f1_score
+from torchsummary import summary
 
 ## Computer devide detection function
 def identify_device():
     device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
     print(f"Using device: {device}")
     return device
+
+def display_model(summary_model, device):
+    if device.type == "cuda":
+        summary_model = summary_model.to(device)
+        summary(summary_model, input_size=(1, 1024))
+    elif device.type == "mps":
+        summary_model = summary_model  # no .to(device) for MPS
+        summary(summary_model, input_size=(1, 1024))
+    else:
+        summary_model = summary_model.to(device)
+        summary(summary_model, input_size=(1, 1024))
 
 
 def print_metrics(all_labels, all_preds):
